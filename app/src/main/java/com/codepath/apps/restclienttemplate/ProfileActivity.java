741b0e038ddc06514c5b18e6475c89469e3eaa79
Mindy Van <mmv0321@gmail.com>
2021-07-01 19:18:58 -0500

    customizing profile, added walkthrough

--- app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
@@ -1,16 +1,23 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.apps.restclienttemplate.models.User;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,6 +41,11 @@ public class ProfileActivity extends AppCompatActivity {
     RecyclerView rvTweets;
     TweetsAdapter adapter;
 
+    ImageView ivBanner;
+    ImageView ivProfile;
+
+    FloatingActionButton fabCompose;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +55,8 @@ public class ProfileActivity extends AppCompatActivity {
         user = (User) Parcels.unwrap(getIntent().getParcelableExtra("user"));
         Log.d(TAG, "Showing profile for " + user.name);
 
+        // init/find things
+        fabCompose = findViewById(R.id.fabCompose);
         client = TwitterApp.getRestClient(this);
         rvTweets = findViewById(R.id.rvProfileTweets);
 
@@ -64,6 +78,25 @@ public class ProfileActivity extends AppCompatActivity {
                 fetchTimelineAsync(0);
             }
         });
+
+        // set up fabCompose
+//        fabCompose.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // compose icon has been selected
+//                Log.d(TAG, "compose a tweet clicked");
+//                // navigate to compose activity
+//                Intent intent = new Intent(ProfileActivity.this, ComposeActivity.class);
+//                startActivityForResult(intent, REQUEST_CODE);
+//            }
+//        });
+
+        // profile
+        ivBanner = findViewById(R.id.ivBanner);
+        Glide.with(this).load(user.publicBannerUrl).into(ivBanner);
+
+        ivProfile = findViewById(R.id.ivProfileImageActual);
+        Glide.with(this).load(user.publicImageUrl).transform(new CircleCrop()).into(ivProfile);
     }
 
     public void fetchTimelineAsync(int page) {
@@ -96,7 +129,7 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
-    // getting the home time line
+    // getting the profile time line
     private void populateProfileTimeline() {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             @Override
@@ -119,5 +152,25 @@ public class ProfileActivity extends AppCompatActivity {
                 Log.e(TAG, "onFailure!" + response, throwable);
             }
         });
+    }
+
+    // when a tweet is sent back as a result is created, INSERT a new tweet into arraylist
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check request code is same as one passed in
+        // and result code (android thing) is valid
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            // use data parameter
+            //Tweet tweet = (Tweet) data.getSerializableExtra("tweet");
+            Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+
+            // modify data source of tweets
+            tweets.add(0, tweet);
+            // update adapter
+            adapter.notifyItemInserted(0);
+            rvTweets.scrollToPosition(0);
+            Log.i(TAG, "posting tweet successful!");
+        }
     }
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -9,7 +9,14 @@ public class User {
 
     public String name;
     public String screenName;
-    public String publicImageUrl;
+    public String publicImageUrl; // profile pic
+
+    // TODO: add
+    public String description; // nullable
+    public boolean verified;
+    public int followersCount;
+    public int followingCount;
+    public String publicBannerUrl;
 
     // empty constructor needed by Parceler Library
     public User() {}
@@ -20,6 +27,13 @@ public class User {
         user.name = jsonObject.getString("name");
         user.screenName = jsonObject.getString("screen_name");
         user.publicImageUrl = jsonObject.getString("profile_image_url_https");
+
+        user.description = jsonObject.getString("description");
+        user.verified = jsonObject.getBoolean("verified");
+        user.followersCount = jsonObject.getInt("followers_count");
+        user.followingCount = jsonObject.getInt("friends_count");
+        user.publicBannerUrl = jsonObject.getString("profile_banner_url");
+
         return user;
     }
 }
